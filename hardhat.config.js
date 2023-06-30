6c4236e517240303f0a9752d7726cbd9b60c61a5
Ronak <ronak01.raj@gmail.com>
2023-06-30 14:01:05 +0530

    hh

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: '0.8.18',
+  networks: {
+    goerli: {
+      url: 'YOUR_QUICKNODE_API_URL',
+      accounts: ['YOUR_PRIVATE_GOERLI_ACCOUNT_KEY'],
+    },
+  },
 };
