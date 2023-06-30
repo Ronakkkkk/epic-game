18998a07d3357c72b2c28479b94bd1ed1002445c
Ronak <ronak01.raj@gmail.com>
2023-06-30 14:01:14 +0530

    jj

--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,9 @@
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
     const gameContract = await gameContractFactory.deploy(
-      ["Leo", "Aang", "Pikachu"],       // Names
+      ["Leo", "Rocket", "Pikachu"],       // Names
       ["https://i.imgur.com/pKd5Sdk.png", // Images
-      "https://i.imgur.com/xVu4vFL.png", 
+      "https://i.imgur.com/e9UBsyr.jpeg", 
       "https://i.imgur.com/WMB6g9u.png"],
       [100, 200, 300],                    // HP values
       [100, 50, 25]                       // Attack damage values
