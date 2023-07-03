273e556ecc509b51a35af7d22603269e4ac5ab83
Ronak <ronak01.raj@gmail.com>
2023-07-03 11:06:37 +0530

    Deployed

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: '0.8.18',
   networks: {
     goerli: {
-      url: 'YOUR_QUICKNODE_API_URL',
-      accounts: ['YOUR_PRIVATE_GOERLI_ACCOUNT_KEY'],
+      url: 'https://frequent-aged-wind.ethereum-goerli.discover.quiknode.pro/2af50dc8a0641eec41b26c481bc67a53735d2aea/',
+      accounts: ['3c4577ae5a08e4862fbb8113e44f29b14d1890f5ecfc89cfd92a044705a70513'],
     },
   },
 };
