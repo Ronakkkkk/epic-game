e2da36c498da67248ae3791267dfeef39901ead4
Ronak <ronak01.raj@gmail.com>
2023-07-26 02:49:08 +0530

    dotnev implementation

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: '0.8.18',
   networks: {
     goerli: {
-      url: 'https://frequent-aged-wind.ethereum-goerli.discover.quiknode.pro/2af50dc8a0641eec41b26c481bc67a53735d2aea/',
-      accounts: ['3c4577ae5a08e4862fbb8113e44f29b14d1890f5ecfc89cfd92a044705a70513'],
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY]
     },
   },
 };
